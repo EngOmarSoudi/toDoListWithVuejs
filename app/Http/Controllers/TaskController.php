9176ada352284e6eb9214eea6ec3dc40cb654f36
EngOmarSoudi <92035173+EngOmarSoudi@users.noreply.github.com>
2022-11-29 23:10:26 +0300

    back-end update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(){
+        return Task::orderBy('created_at','DESC')->get();
+    }
+    public function store(Request $request){
+        $newTask=new Task;
+        $newTask::create([
+            'title' => $request->Task['title'],
+            'date' => $request->Task['date'],
+            'time' => $request->Task['time'],
+            'completed' => $request->Task['completed'],
+            'details' => $request->Task['details'],
+            'completed_at' => '2022-11-10 06:06:27',
+            'user_id' => $request->Task['user_id'],
+        ]) ;
+
+        return $newTask;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TaskController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/tasks',[TaskController::class,'index']);
+Route::prefix('/task')->group(function(){
+   Route::post('/store',[TaskController::class,'store']);
+   Route::put('/{id}',[TaskController::class,'update']);
+   Route::delete('/{id}',[TaskController::class,'destroy']);
 });
